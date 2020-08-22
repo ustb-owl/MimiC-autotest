682f71bb27ef94ab2ce4e349d4f47041bf5a42da
MaxXSoft <x@MaxXSoft.net>
2020-08-22 15:17:57 +0800

    (run) added new test cases

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ import sys
 dirs = [
     'sysyruntimelibrary/section1/functional_test',
     'sysyruntimelibrary/section1/performance_test',
+    'sysyruntimelibrary/section2/performance_test',
 ]
 # path to MimiC compiler
 mmcc = '../../build/mmcc'
